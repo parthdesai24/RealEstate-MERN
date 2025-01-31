2e569b410e5d57da17d69935a649089ae92cfca5
parthdesai24 <parth121110@gmail.com>
2025-01-31 22:42:28 +0530

    changes added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -11,7 +11,7 @@ import jwtCheck from "../config/auth0Config.js";
 
 const router = express.Router();
 
-router.post("/register", createUser);
+router.post("/register", jwtCheck, createUser);
 router.post("/bookVisit/:id", jwtCheck, bookVisit);
 router.post("/allBookings", allBookings);
 router.post("/removeBooking/:id", jwtCheck, cancelBooking);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
     authorizationParams={{
       redirect_uri: "https://estatemap.vercel.app",
     }}
-    audience="https://estatemap.vercel.app.api"
+    audience="http://localhost:3000"
     scope="openid profile email"
   >
     <MantineProvider>
